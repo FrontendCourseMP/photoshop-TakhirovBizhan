a709e8906d9371befe517ae60d64a299c5d66d06
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2026-05-26 19:09:06 +0300

    feat: чутка поправил структуру и накинул общие стили

---
+++ src/app/App.tsx
@@ -0,0 +1,6 @@
+import type { JSX } from "react";
+import { ImageEditorPage } from "../pages/ImageEditorPage/ImageEditorPage";
+
+export default function App(): JSX.Element {
+  return <ImageEditorPage />;
+}
